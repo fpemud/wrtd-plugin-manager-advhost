7fc772de1d97bd310037ba7b365b02d6b5ff064e
fpemud <fpemud@sina.com>
2017-08-09 07:32:56 +0800

    fix

--- manager_advhost/__init__.py
+++ manager_advhost/__init__.py
@@ -30,24 +30,31 @@ class _PluginObject:
         self.param = data
         self.logger = logging.getLogger(self.__module__ + "." + self.__class__.__name__)
 
+        # register endpoint factory
+        fac = EndPointFactory(self)
+        self.param.managers["apiserver"].register_endpoint_factory("advhost", fac)
+        self.param.managers["apiserver"].register_endpoint_factory("gwim", fac)
+
     def dispose(self):
         pass
 
-    def manager_initialized(self, name):
-        if name == "apiserver":
-            self.param.managers[name].register_endpoint_factory("advhost", ApiServerEndPointFactory(self))
+    def get_router_info(self):
+        return dict()
 
 
-class ApiServerEndPointFactory:
+class EndPointFactory:
 
     def __init__(self, pObj):
         self.pObj = pObj
 
-    def new_endpoint(self, local_ip, local_port, peer_ip, peer_port, sproc):
-        return ApiServerEndPoint(self.pObj, peer_ip, sproc)
+    def new_endpoint(self, channel, local_ip, local_port, peer_ip, peer_port, sproc):
+        if channel == "advhost":
+            return AdvHostEndPoint(self.pObj, peer_ip, sproc)
+        elif channel == "gwim":
+            return GwImEndPoint(self.pObj, peer_ip, sproc)
 
 
-class ApiServerEndPoint:
+class AdvHostEndPoint:
 
     def __init__(self, pObj, peer_ip, sproc):
         self.pObj = pObj
@@ -58,23 +65,26 @@ class ApiServerEndPoint:
         # send reply
         data2 = dict()
         data2["network-list"] = []
-        data2["client-list"] = []
+        data2["host-list"] = []
         return data2
 
-    def close(self):
+    def close2(self):
         pass
 
-    def on_notification_network_list_change(self, data):
+    def on_command_get_network_list(self, data, return_callback, error_callback):
         pass
 
-    def on_notification_client_add(self, data):
-        pass
-
-    def on_notification_client_change(self, data):
-        pass
-
-    def on_notification_client_remove(self, data):
+    def on_command_get_host_list(self, data, return_callback, error_callback):
         pass
 
     def on_command_wakeup(self, data, return_callback, error_callback):
         pass
+
+
+
+class GwImEndPoint:
+
+    def __init__(self, pObj, peer_ip, sproc):
+        self.pObj = pObj
+        self.peer_ip = peer_ip
+        self.sproc = sproc
