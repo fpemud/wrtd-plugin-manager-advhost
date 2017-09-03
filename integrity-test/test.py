b0adcf5376237caeccb5b26cdff44e0c5e4e129b
fpemud <fpemud@sina.com>
2017-09-04 07:19:18 +0800

    fix

---
+++ integrity-test/test.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+import re
+import json
+import msghole
+import subprocess
+from gi.repository import Gio
+from gi.repository import GLib
+
+
+class Client(msghole.EndPoint):
+
+    def __init__(self, conn):
+        super().__init__()
+        self.set_iostream_and_start(conn)
+
+    def on_error(self, excp):
+        print("on_error: " + str(excp))
+
+    def on_close(self):
+        print("on_close")
+
+    def on_notification_host_add(self, data):
+        print("on_notification_host_add: " + json.dumps(data))
+
+    def on_notification_host_change(self, data):
+        print("on_notification_host_change: " + json.dumps(data))
+
+    def on_notification_host_remove(self, data):
+        print("on_notification_host_remove: " + json.dumps(data))
+
+    def on_notification_network_list_change(self, data):
+        print("on_notification_network_list_change: " + json.dumps(data))
+
+
+def getGatewayIpAddress():
+    ret = subprocess.check_output(["/bin/route", "-n4"]).decode("utf-8")
+    # syntax: DestIp GatewayIp DestMask ... OutIntf
+    m = re.search("^(0\\.0\\.0\\.0)\\s+([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+)\\s+(0\\.0\\.0\\.0)\\s+.*\\s+(\\S+)$", ret, re.M)
+    if m is None:
+        return None
+    return m.group(2)
+
+
+def idleInvoke(func, *args):
+    def _idleCallback(func, *args):
+        func(*args)
+        return False
+    GLib.idle_add(_idleCallback, func, *args)
+
+
+def getHostList(client):
+    print("getHostList begin")
+    client.exec_command("get-host-list",
+                        return_callback=command_get_host_list_return_cb,
+                        error_callback=command_get_host_list_error_cb)
+    print("getHostList end")
+
+
+def command_get_host_list_return_cb(data):
+    print("on_command_get_host_list_return: " + json.dumps(data))
+    idleInvoke(getNetworkList, client)
+
+
+def command_get_host_list_error_cb(reason):
+    print("on_command_get_host_list_error:" + reason)
+    idleInvoke(getNetworkList, client)
+
+
+def getNetworkList(client):
+    print("getNetworkList begin")
+    client.exec_command("get-network-list",
+                        return_callback=command_get_network_list_return_cb,
+                        error_callback=command_get_network_list_error_cb)
+    print("getNetworkList end")
+
+
+def command_get_network_list_return_cb(data):
+    print("on_command_get_network_list_return: " + json.dumps(data))
+
+
+def command_get_network_list_error_cb(reason):
+    print("on_command_get_network_list_error:" + reason)
+
+
+if __name__ == "__main__":
+    print("Progress1")
+    sc = Gio.SocketClient.new()
+    sc.set_family(Gio.SocketFamily.IPV4)
+    sc.set_protocol(Gio.SocketProtocol.TCP)
+
+    print("Progress2")
+    conn = sc.connect_to_host(getGatewayIpAddress(), 2222)
+
+    print("Progress3")
+    client = Client(conn)
+    try:
+        print("Progress4")
+        idleInvoke(getHostList, client)
+        GLib.MainLoop().run()
+    finally:
+        print("Progress5")
+        client.close()
